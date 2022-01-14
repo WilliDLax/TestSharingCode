5383c61c8ff3c8cfc7bfe17a2adef893ac24275f
wemabank <shalomwill98@gmail.com>
2022-01-14 09:24:09 +0100

    Give Cliff

---
+++ SharedCode/TestSharing.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace SharedCode
+{
+    public class TestSharing
+    {
+        public TestSharing()
+        {
+        }
+
+        public static void LogSomething()
+        {
+            Console.WriteLine("Yes it works");
+        }
+    }
+}
